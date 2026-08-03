a92129884977af9791e854afc64edc19dfeba506
volkova <katrinavolk37695@gmail.com>
2026-08-03 15:00:30 +0300

    Add composer.json

---
+++ src/SpeakWithThomasHardy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace User1872\ComposerUse;
+
+use Small4ange\KatkaVComposer\Dota2;
+
+class SpeakWithThomasHardy
+{
+    public function main() : string
+    {
+        return (new \Small4ange\KatkaVComposer\Dota2)->say(0);
+    }
+
+}
